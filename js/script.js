2204fcb5fcab701cc2e0ba99d4eecc78091bc87d
Illia <example@gmail.com>
2023-06-15 18:17:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+// const category = 'toys';
+
+// // console.log('http://someurl.com/' + category)
+// console.log(`http://someurl.com/${category}/5`)
+
+// const user = 'Illia';
+// alert(`Hi, ${user}`)
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('18 years old?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// const userName = 'John';
+// let userNumber = 25;
+// userNumber = 24;
+// console.log(userNumber);
